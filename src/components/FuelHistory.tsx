6b8a665884d9beba399b9186b3e94c7bafe9f1b7
Mansive <xmansive@gmail.com>
2023-10-13 21:06:12 -0500

    Add message for when user has no fuel history

--- src/components/FuelHistory.tsx
+++ src/components/FuelHistory.tsx
@@ -13,6 +13,7 @@ interface History {
 const FuelHistory = () => {
   const [expandedIds, setExpandedIds] = useState<number[]>([]);
   const [fuelHistoryData, setFuelHistoryData] = useState<History[]>([]);
+  const [fuelHistoryDataExists, setFuelHistoryDataExists] = useState(false);
   const token = localStorage.getItem("token");
 
   useEffect(() => {
@@ -24,6 +25,7 @@ const FuelHistory = () => {
           },
         });
         setFuelHistoryData(response.data);
+        setFuelHistoryDataExists(true);
       } catch (error) {
         console.error(error);
       }
@@ -56,67 +58,71 @@ const FuelHistory = () => {
 
   return (
     <>
-      {fuelHistoryData.map((history) => (
-        <div key={history.id} className="client-history-item">
-          <button
-            className="date"
-            onClick={() => toggleExpand(history.id)}
-            aria-expanded={expandedIds.includes(history.id)}
-            aria-controls={`details-${history.id}`}
-          >
-            {history.date_requested} delivery date: {history.delivery_date}
-          </button>
-          {expandedIds.includes(history.id) && (
-            <div className="details">
-              <div>
-                <label htmlFor="gallons_requested">Gallons requested:</label>
-                <input
-                  style={greyedOutStyle}
-                  type="text"
-                  value={history.gallons_requested}
-                  readOnly
-                />
+      {!fuelHistoryDataExists ? (
+        <div>You do not have any fuel history</div>
+      ) : (
+        fuelHistoryData.map((history) => (
+          <div key={history.id} className="client-history-item">
+            <button
+              className="date"
+              onClick={() => toggleExpand(history.id)}
+              aria-expanded={expandedIds.includes(history.id)}
+              aria-controls={`details-${history.id}`}
+            >
+              {history.date_requested} delivery date: {history.delivery_date}
+            </button>
+            {expandedIds.includes(history.id) && (
+              <div className="details">
+                <div>
+                  <label htmlFor="gallons_requested">Gallons requested:</label>
+                  <input
+                    style={greyedOutStyle}
+                    type="text"
+                    value={history.gallons_requested}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label htmlFor="delivery_date">Delivery Date:</label>
+                  <input
+                    style={greyedOutStyle}
+                    type="text"
+                    value={history.delivery_date}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label htmlFor="delivery_address">Delivery Address</label>
+                  <input
+                    style={greyedOutStyle}
+                    type="text"
+                    value={history.delivery_address}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label htmlFor="suggested_price">Suggested Price</label>
+                  <input
+                    style={greyedOutStyle}
+                    type="text"
+                    value={history.suggested_price}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label htmlFor="total_price">Total Price</label>
+                  <input
+                    style={greyedOutStyle}
+                    type="text"
+                    value={totalCost}
+                    readOnly
+                  />
+                </div>
               </div>
-              <div>
-                <label htmlFor="delivery_date">Delivery Date:</label>
-                <input
-                  style={greyedOutStyle}
-                  type="text"
-                  value={history.delivery_date}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label htmlFor="delivery_address">Delivery Address</label>
-                <input
-                  style={greyedOutStyle}
-                  type="text"
-                  value={history.delivery_address}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label htmlFor="suggested_price">Suggested Price</label>
-                <input
-                  style={greyedOutStyle}
-                  type="text"
-                  value={history.suggested_price}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label htmlFor="total_price">Total Price</label>
-                <input
-                  style={greyedOutStyle}
-                  type="text"
-                  value={totalCost}
-                  readOnly
-                />
-              </div>
-            </div>
-          )}
-        </div>
-      ))}
+            )}
+          </div>
+        ))
+      )}
     </>
   );
 };
