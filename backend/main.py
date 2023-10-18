7389cc4295e4429885ed0976bd8c7089facd28e4
Mansive <xmansive@gmail.com>
2023-10-18 00:56:10 -0500

    Prepare for pytest database override

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ XATA_BRANCH: str = os.getenv("XATA_BRANCH")
 DB_URL: str = os.getenv("DB_URL")
 
 # Loading DB
-db = XataClient(db_url=DB_URL, api_key=XATA_API_KEY, branch_name=XATA_BRANCH)
+#db = XataClient(db_url=DB_URL, api_key=XATA_API_KEY, branch_name=XATA_BRANCH)
 app = FastAPI()
 
 # Allow CORS
@@ -44,7 +44,7 @@ class User(BaseModel):
     def username_alphanumeric(cls, v):
         assert v.isalnum(), "must be alphanumeric"
         return v
-    
+
 
 states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
            'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
@@ -86,6 +86,11 @@ class FuelData(BaseModel):
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
 
 
+def get_db():
+    db = XataClient(db_url=DB_URL, api_key=XATA_API_KEY, branch_name=XATA_BRANCH)
+    yield db
+
+
 # Not implementing it yet
 def calculate_price() -> float:  # type: ignore
     pass
@@ -115,7 +120,7 @@ def decode_token(token: str):
 
 
 @app.post("/api/register", description="Register a new user")
-async def register(user: User):
+async def register(user: User, db: XataClient = Depends(get_db)):
     # Check if the user exists
     response = db.sql().query(
         'SELECT * FROM "Users" WHERE username = $1', [user.username]
@@ -136,7 +141,7 @@ async def register(user: User):
 
 
 @app.post("/api/token", description="Get user token")
-async def login(data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+async def login(data: Annotated[OAuth2PasswordRequestForm, Depends()], db: XataClient = Depends(get_db)):
     response = db.sql().query(
         'SELECT username, password, require_details FROM "Users" WHERE username = $1 LIMIT 1',
         [data.username],
@@ -161,7 +166,7 @@ async def login(data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
 
 @app.post("/api/user/", description="Adds user details")
-async def add_user_details(details: UserDetails, token: str = Depends(oauth2_scheme)):
+async def add_user_details(details: UserDetails, token: str = Depends(oauth2_scheme), db: XataClient = Depends(get_db)):
     user = decode_token(token)
     response = db.sql().query(
         'SELECT require_details FROM "Users" WHERE username = $1', [user]
@@ -190,7 +195,7 @@ async def add_user_details(details: UserDetails, token: str = Depends(oauth2_sch
 
 @app.put("/api/user/", description="Updates user details")
 async def update_user_details(
-    details: UserDetails, token: str = Depends(oauth2_scheme)
+    details: UserDetails, token: str = Depends(oauth2_scheme), db: XataClient = Depends(get_db)
 ):
     user = decode_token(token)
     response = db.sql().query(
@@ -198,7 +203,7 @@ async def update_user_details(
     )
     if len(response) != 1:
         raise HTTPException(status_code=400, detail="User not registered")
-    
+
     if response["records"][0]["require_details"] == True:
         raise HTTPException(status_code=400, detail="User details not registered")
 
@@ -219,7 +224,7 @@ async def update_user_details(
 
 
 @app.get("/api/user/", description="Returns user details")
-async def get_user_details(token: str = Depends(oauth2_scheme)):
+async def get_user_details(token: str = Depends(oauth2_scheme), db: XataClient = Depends(get_db)):
     user = decode_token(token)
 
     response = db.sql().query(
@@ -229,10 +234,10 @@ async def get_user_details(token: str = Depends(oauth2_scheme)):
 
     if len(response) != 1:
         raise HTTPException(status_code=400, detail="User not registered")
-    
+
     if response["records"][0]["require_details"] == True:
         raise HTTPException(status_code=400, detail="User details not registered")
-    
+
     record = response["records"][0]
 
     details = UserDetails(
@@ -248,7 +253,7 @@ async def get_user_details(token: str = Depends(oauth2_scheme)):
 
 
 @app.post("/api/fuel_quote/", description="Adds a fuel quote")
-async def add_fuel_quote(data: FuelData, token: str = Depends(oauth2_scheme)):
+async def add_fuel_quote(data: FuelData, token: str = Depends(oauth2_scheme), db: XataClient = Depends(get_db)):
     user = decode_token(token)
     response = db.sql().query(
         'SELECT full_name, address1, address2, city, state, zipcode, require_details FROM "Users" WHERE username = $1',
@@ -278,7 +283,7 @@ async def add_fuel_quote(data: FuelData, token: str = Depends(oauth2_scheme)):
         + " "
         + details.zipcode
     )
-    
+
     cst = pytz.timezone("America/Chicago")
     current_time_cst = datetime.now(cst)
     data.date_requested = current_time_cst.isoformat()
@@ -308,7 +313,7 @@ async def add_fuel_quote(data: FuelData, token: str = Depends(oauth2_scheme)):
 
 
 @app.get("/api/fuel_quote/", description="Returns a list of fuel quotes")
-async def get_fuel_quote(token: str = Depends(oauth2_scheme)):
+async def get_fuel_quote(token: str = Depends(oauth2_scheme), db: XataClient = Depends(get_db)):
     user = decode_token(token)
     details = db.sql().query(
         'SELECT require_details FROM "Users" WHERE username = $1', [user]
